fe6878cf8bdf7d8d0d84da9db398a38db1089ce6
Seyedbehnam Rad <behnamrad.br@gmail.com>
2022-11-11 03:07:21 +0330

    assignment_final1 has been obtained by all queries
    neighbordhood_tree_most only has top 5 trees of each neighborhood, neighbordhood_tree_total has the total number of trees of each neighborhood

--- js/waffle_chart.js
+++ js/waffle_chart.js
@@ -1,15 +1,17 @@
+const neighborhood_tree_most = './data/neighborhood_tree_most.csv';
+const neighborhood_tree_total = './data/neighborhood_tree_total.csv';
+width=50;
 
+const svg = d3.create('svg').attr('viewBox', [0, 0, width, 200]);
+for(let i=0;i<=3;i++)
+{const svg = d3.create('svg').attr('viewBox', [0, 0, width, 200]);}
+            // d3.select('svg')
+            //     .style('color','red')
+            //     .append('circle')
+            //     .attr('cx', '50%')
+            //     .attr('cy', '50%')
+            //     .attr('r', 20)
+            //     .style('fill', 'green')
+            //     .text('fuck');
 
-var height=500,width=500;
-
-var svg=d3.select('svg')
-    .attr('width',width )
-    .attr('height',height)
-    .attr('border',4)
-    .attr('color',red)
-    .append('p')
-    .text('helllo')
-var waffle_chart=d3.select('rect')
-    .data('~/data/tree_processed_location.csv')
-    .enter()
-    .append('rect')
+       
