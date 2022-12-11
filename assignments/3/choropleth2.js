98228285033ee03b818564ada89018418f1ed0dd
siamackSeifi <siamack.seifi@gmail.com>
2022-12-12 03:16:31 +0330

    development of assignment3 chart2

--- assignments/3/choropleth2.js
+++ assignments/3/choropleth2.js
@@ -4,23 +4,34 @@ import * as Chart from "/lib/charts.js";
 const circoscrizioni = (await d3.json("/data/circoscrizioni.json"));
 const data = await d3.json("/data/new.json");
 const featuresList = data.features;
+let areas = [];
+
+circoscrizioni.features.forEach((element) => {
+    areas.push({
+        "neighborhood": element.properties.nome,
+        "area": Math.floor(Math.abs(
+            d3.polygonArea(element.geometry.coordinates[0])) * Math.pow(10, 10)
+        )
+    })
+});
 
 var result = d3.group(featuresList.slice(0, -1), d => d.properties.neighborhood);
 var finalData = [];
 result.forEach((value, key) => {
     finalData.push({
         neighborhood: key,
-        abundance: d3.count(value, d => d.properties["Tree ID"])
+        density: d3.sum(value, d => d.properties["Canopy Cover (m2)"]) /
+            (areas.filter(e => {return e.neighborhood === key}))[0].area
     });
 });
 
 const choroplethChartSVG = await Chart.Choropleth(finalData, {
     id: d => d.neighborhood,
-    value: d => d.abundance,
+    value: d => d.density,
     scale: d3.scaleQuantize,
-    domain: [38, 3024],
+    domain: [0.000038636892037286005, 0.013113484],
     range: d3.schemeGreens[5],
-    title: (f, d) => `neighborhood: ${d?.neighborhood}, \n abundance: ${d?.abundance}`,
+    title: (f, d) => `neighborhood: ${d?.neighborhood}, \n density: ${d?.density}`,
     features: circoscrizioni,
     featureId: d => d.neighborhood,
     // borders: statemesh,
@@ -28,4 +39,4 @@ const choroplethChartSVG = await Chart.Choropleth(finalData, {
     height: 610
 })
 
-document.getElementById("chropleth1").appendChild(choroplethChartSVG);
+document.getElementById("chropleth2").appendChild(choroplethChartSVG);
