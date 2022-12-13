cbaf11b433329492dc0f34dd1fe568cfa606d9a6
majidjaved1994 <116609787+majidjaved1994@users.noreply.github.com>
2022-12-14 00:24:42 +0100

    Update histogram.js

--- assignments/2/histogram.js
+++ assignments/2/histogram.js
@@ -1,8 +1,8 @@
-
+function drawChart_histogram() {
     const el = "#histogram";
 
     // Data
-    d3.json("../../data/dataPreProcess/assignment2_final.json").then(function (dataset) {
+    d3.json("../../data_clean/histogram.json").then(function (dataset) {
 
         // Dimensions
         let dimensions = {
@@ -171,3 +171,4 @@
     });
 }
 
+drawChart_histogram();
