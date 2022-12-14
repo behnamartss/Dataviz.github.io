1a0c8482bb2db4256fdd5c3c7a8a73ced399bc1a
majidjaved1994 <116609787+majidjaved1994@users.noreply.github.com>
2022-12-14 03:21:27 +0100

    Update histogram.js

--- assignments/2/histogram.js
+++ assignments/2/histogram.js
@@ -1,4 +1,4 @@
-d3.csv("../../data/dataPreProcess/assignment2_histogram.csv", function (dataset) {
+d3.json("../../data/dataPreProcess/assignment2_final.json").then (function (dataset) {
 		
 
         // Dimensions
