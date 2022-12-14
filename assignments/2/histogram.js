cfdfde15f64191e559d0feb922cb890e0674790e
majidjaved1994 <116609787+majidjaved1994@users.noreply.github.com>
2022-12-14 02:30:56 +0100

    Update histogram.js

--- assignments/2/histogram.js
+++ assignments/2/histogram.js
@@ -1,3 +1,4 @@
+<script>
 d3.csv("../../data/dataPreProcess/assignment2_histogram.csv").get (function (dataset) {
 		
 
@@ -166,3 +167,4 @@ d3.csv("../../data/dataPreProcess/assignment2_histogram.csv").get (function (dat
         // default metric
         histogram(key_values[0]);
 		});
+</script>
